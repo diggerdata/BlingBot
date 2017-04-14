23f1284a730cf3d0fcdf2ba284c8f96e2a5f7134
diggerdata <robotincad@gmail.com>
2017-04-14 01:12:07 -0400

    Added cmd_vel_subscriber

---
+++ src/bling_bot/scripts/cmd_vel_subscriber.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest('bling_bot')
+import rospy
+import time
+import math
+import PID as pid
+import tf.transformations
+from geometry_msgs.msg import Twist
+from dual_mc33926_rpi import motors, MAX_SPEED
+
+
+def limit(num, minimum=-480, maximum=480):
+    return max(min(num, maximum), minimum)
+
+def encoder_callback(msg):
+    global left_vel
+    global right_vel
+
+    left_vel = msg.linear.x
+    right_vel = msg.linear.y
+
+def cmd_callback(msg):
+    global left_vel
+    global right_vel
+
+    rospy.loginfo("Received a /cmd_vel message!")
+    rospy.loginfo("Linear Components: [%f, %f, %f]"%(msg.linear.x, msg.linear.y, msg.linear.z))
+    rospy.loginfo("Angular Components: [%f, %f, %f]"%(msg.angular.x, msg.angular.y, msg.angular.z))
+
+    robot_width = 0.17
+
+    # Do velocity processing here:
+    # Use the kinematics of your robot to map linear and angular velocities into motor commands
+
+    kp = 0.2
+    ki = 0.0
+    kd = 0.0
+    sample_time = 0.01
+
+    vl = msg.linear.x - msg.angular.z * robot_width / 2.0
+    vr = msg.linear.x + msg.angular.z * robot_width / 2.0
+
+    pid_left = pid.pid(kp, ki, kd)
+    pid_right = pid.pid(kp, ki, kd)
+
+    pid_left.SetPoint = vl
+    pid_right.SetPoint = vr
+
+    pid_left.setSampleTime(sample_time)
+    pid_right.setSampleTime(sample_time)
+
+    pid_left.update(left_vel)
+    pid_right.update(right_vel)
+
+    vl_out = limit(pid_left.output)
+    vr_out = limit(pid_right.output)
+
+    # Then set your wheel speeds (using wheel_left and wheel_right as examples)
+    try:
+        motors.setSpeeds(vl, vr)
+
+    finally:
+        motors.setSpeeds(0, 0)
+        motors.disable()
+
+def listener():
+    global left_vel
+    global right_vel
+
+    rospy.init_node('cmd_vel_subscriber')
+    motors.enable()
+    motors.setSpeeds(0, 0)
+    rospy.Subscriber("/cmd_vel", Twist, cmd_callback)
+    rospy.Subscriber("/ard_odom", Twist, encoder_callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
