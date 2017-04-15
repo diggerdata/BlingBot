a44f06f03fa2f899c3e43f6049c6f1c2bc3d2db1
diggerdata <robotincad@gmail.com>
2017-04-14 20:04:32 -0400

    Push

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-export ROS_MASTER_URI=http://ripnate.dyn.wpi.edu:11311
-export ROS_IP=ripnate.dyn.wpi.edu
+export ROS_MASTER_URI=http://natepc.dyn.wpi.edu:11311
+export ROS_IP=natepc.dyn.wpi.edu
 
 gnome-terminal -e rviz
 roslaunch bling_bot bling.launch
