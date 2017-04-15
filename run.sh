867d5d49de575641144f33ba739a0a0079346cb3
diggerdata <robotincad@gmail.com>
2017-04-14 21:44:55 -0400

    Stuff

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 export ROS_MASTER_URI=http://natepc.dyn.wpi.edu:11311
 export ROS_IP=natepc.dyn.wpi.edu
 
-roslaunch turtlebot_teleop keyboard_teleop.launch
-
-# gnome-terminal -e rviz
-gnome-terminal -e roslaunch bling_bot bling.launch
+gnome-terminal -e rviz
+roslaunch bling_bot bling.launch
