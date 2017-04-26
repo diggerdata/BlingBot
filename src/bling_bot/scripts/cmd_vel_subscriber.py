8921616fb721fbe6f21cf58008d832b5d04347e0
diggerdata <robotincad@gmail.com>
2017-04-26 15:39:25 -0400

    Fixed indentation

--- src/bling_bot/scripts/cmd_vel_subscriber.py
+++ src/bling_bot/scripts/cmd_vel_subscriber.py
@@ -11,16 +11,15 @@ from dual_mc33926_rpi import motors, MAX_SPEED
 
 
 def limit(num, minimum=-480, maximum=480):
-    return max(min(num, maximum), minimum)
-
+	return max(min(num, maximum), minimum)
 
 def set_motor_vel(vl, vr):
-    try:
-        motors.setSpeeds(vl, vr)
+	try:
+		motors.setSpeeds(vl, vr)
 
-    except:
-        motors.setSpeeds(0, 0)
-        motors.disable()
+	except:
+		motors.setSpeeds(0, 0)
+		motors.disable()
 
 
 def encoder_callback(msg):
@@ -65,56 +64,56 @@ def flame_callback(msg):
 		set_motor_vel(0, 0)
 
 def main():
-    rospy.init_node('cmd_vel_subscriber')
+	rospy.init_node('cmd_vel_subscriber')
 
-    global first_time
-    global left_vel
-    global right_vel
-    global last_time
-    global vl
-    global vr
-    global pid_left
-    global pid_right
+	global first_time
+	global left_vel
+	global right_vel
+	global last_time
+	global vl
+	global vr
+	global pid_left
+	global pid_right
 	global flame_stop
 
-    kp = 1.0
-    ki = 1700.0
-    kd = 0.5
-    sample_time = 0.009
+	kp = 1.0
+	ki = 1700.0
+	kd = 0.5
+	sample_time = 0.009
 
-    pid_left = pid.pid(kp, ki, kd)
-    pid_right = pid.pid(kp, ki, kd)
+	pid_left = pid.pid(kp, ki, kd)
+	pid_right = pid.pid(kp, ki, kd)
 
 	flame_stop = False
 
-    # pid_left.setSampleTime(sample_time)
-    # pid_right.setSampleTime(sample_time)
+	# pid_left.setSampleTime(sample_time)
+	# pid_right.setSampleTime(sample_time)
 
-    vl = 0.0
-    vr = 0.0
+	vl = 0.0
+	vr = 0.0
 
-    left_vel = 0.0
-    right_vel = 0.0
+	left_vel = 0.0
+	right_vel = 0.0
 
-    first_time = True
+	first_time = True
 
-    if first_time:
-        rospy.loginfo("MOTORS ARE UP...")
+	if first_time:
+	    rospy.loginfo("MOTORS ARE UP...")
 
-    motors.enable()
-    motors.setSpeeds(0, 0)
+	motors.enable()
+	motors.setSpeeds(0, 0)
 
-    rospy.Subscriber("/ard_odom", Twist, encoder_callback)
-    rospy.Subscriber("/cmd_vel", Twist, cmd_callback)
-    rospy.Subscriber("/flame_seen", Bool, flame_callback)
+	rospy.Subscriber("/ard_odom", Twist, encoder_callback)
+	rospy.Subscriber("/cmd_vel", Twist, cmd_callback)
+	rospy.Subscriber("/flame_seen", Bool, flame_callback)
 
-    first_time = False
-    rospy.spin()
-    #
-    # r = rospy.Rate(100)
-    # while not rospy.is_shutdown():
-    #
-    #     r.sleep()
+	first_time = False
+	rospy.spin()
+	#
+	# r = rospy.Rate(100)
+	# while not rospy.is_shutdown():
+	#
+	#     r.sleep()
 
 if __name__ == '__main__':
-    main()
+	main()
